c1ce7a81c81fe990dff3a8b3cf6a51bbc0cc0407
yosr2003 <yosr.amamou@yahoo.com>
2023-05-28 17:57:01 +0100

    .....

--- script.js
+++ script.js
@@ -59,3 +59,128 @@ sr.reveal('.contact_input',{interval: 200});
 
 
 
+
+
+
+
+/*   API    */
+
+// Function to fetch and populate dropdown options based on search type
+async function populateOptions(searchType) {
+    const searchOptions = document.getElementById('rick-morty-searchOptions');
+    searchOptions.innerHTML = ''; // Clear previous options
+
+    // Function to fetch all pages of data
+    async function fetchAllPages(url) {
+        let allResults = [];
+        let nextPage = url;
+
+        while (nextPage) {
+            const response = await fetch(nextPage);
+            const data = await response.json();
+            const { results, info } = data;
+            allResults.push(...results);
+            nextPage = info.next;
+        }
+
+        return allResults;
+    }
+
+    // Perform the API request based on the selected search type
+    const apiUrl = `https://rickandmortyapi.com/api/${searchType}`;
+    const allData = await fetchAllPages(apiUrl);
+
+    if (allData.length > 0) {
+        if (searchType === 'episode') {
+            // Sort episodes by season and episode number
+            const sortedEpisodes = allData.sort((a, b) => {
+                const [aSeason, aEpisode] = a.episode.split('E');
+                const [bSeason, bEpisode] = b.episode.split('E');
+                if (aSeason !== bSeason) {
+                    return aSeason.localeCompare(bSeason);
+                }
+                return parseInt(aEpisode) - parseInt(bEpisode);
+            });
+
+            let currentSeason = '';
+            sortedEpisodes.forEach(episode => {
+                const [season, episodeNumber] = episode.episode.split('E');
+                if (season !== currentSeason) {
+                    const seasonOption = document.createElement('optgroup');
+                    seasonOption.label = `Season ${season}`;
+                    searchOptions.appendChild(seasonOption);
+                    currentSeason = season;
+                }
+                const option = document.createElement('option');
+                option.value = episode.id;
+                option.text = `Episode ${episodeNumber} - ${episode.name}`;
+                searchOptions.lastElementChild.appendChild(option);
+            });
+        } else {
+            allData.forEach(item => {
+                const option = document.createElement('option');
+                option.value = item.id;
+                option.text = item.name || item.dimension;
+                searchOptions.appendChild(option);
+            });
+        }
+    } else {
+        const option = document.createElement('option');
+        option.disabled = true;
+        option.text = 'No options available.';
+        searchOptions.appendChild(option);
+    }
+}
+
+// Function to handle the search based on selected search type and option
+function rickMortySearch() {
+    const searchType = document.getElementById('rick-morty-searchType').value;
+    const searchOptions = document.getElementById('rick-morty-searchOptions');
+    const selectedOption = searchOptions.options[searchOptions.selectedIndex].value;
+    const searchResult = document.getElementById('rick-morty-searchResult');
+    searchResult.innerHTML = ''; // Clear previous results
+
+    // Perform the API request based on the selected search type and option
+    fetch(`https://rickandmortyapi.com/api/${searchType}/${selectedOption}`)
+        .then(response => response.json())
+        .then(data => {
+            const resultInfo = document.createElement('div');
+            resultInfo.classList.add('animated', 'fadeIn'); // Add animation class
+
+            if (searchType === 'character') {
+                resultInfo.innerHTML = `<h2>${data.name}</h2>
+                                        <div class="character-info">
+                                            <img src="${data.image}" alt="${data.name}" class="character-image" />
+                                            <div>
+                                                <p>Status: ${data.status}</p>
+                                                <p>Species: ${data.species}</p>
+                                                <p>Gender: ${data.gender}</p>
+                                                <p>Origin: ${data.origin.name}</p>
+                                                <p>Location: ${data.location.name}</p>
+                                            </div>
+                                        </div>`;
+            } else if (searchType === 'episode') {
+                resultInfo.innerHTML = `<h2>${data.name}</h2>
+                                        <p>Episode: ${data.episode}</p>
+                                        <p>Air Date: ${data.air_date}</p>`;
+            } else if (searchType === 'location') {
+                resultInfo.innerHTML = `<h2>${data.name}</h2>
+                                        <p>Type: ${data.type}</p>
+                                        <p>Dimension: ${data.dimension}</p>`;
+            }
+            searchResult.appendChild(resultInfo);
+        })
+        .catch(error => {
+            console.error(error);
+            searchResult.textContent = 'An error occurred while fetching data.';
+        });
+}
+
+// Event listener to populate options when search type changes
+document.getElementById('rick-morty-searchType').addEventListener('change', function() {
+    const searchType = this.value;
+    populateOptions(searchType);
+});
+
+// Initial population of options based on default search type
+populateOptions('character');
